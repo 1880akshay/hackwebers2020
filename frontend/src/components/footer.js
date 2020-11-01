5235452711e2b8068b6601abf83573dfcdc63d58
Akshay Jain <1880akshay@gmail.com>
2020-11-01 20:44:43 +0530

    main code

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="text-white text-center" style={{fontSize: 'smaller'}}>Made with &#128151; by: Akshay Jain, IIT Kharagpur</div>
+            </div>
+        )
+    }
+}
